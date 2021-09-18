d9aa5a2a2864d336adfbf0e78b677f46d84ab634
Mostafa Aboutalebi <m.abotalebie@gmail.com>
2021-09-18 17:29:57 +0430

    fix default config

--- src/config/sms.php
+++ src/config/sms.php
@@ -6,7 +6,7 @@ return [
 
     'providers' => [
         'IPPanel' => [
-            'provider' => 'ippanel',
+            'provider' => 'IPPanel',
             'api_key' => env('SMS_API_KEY'),
             'number' => env('SMS_NUMBER'),
         ]
